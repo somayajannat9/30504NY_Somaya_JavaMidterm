dbd141da6bc3f9a5b3a94b018b58191f75fcd10d
somayajannat9 <somayajannat9@gmail.com>
2022-10-10 04:10:56 -0400

    Did Fibonacci.

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -10,5 +10,29 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
+        int fib = 13;
+        int fibArr [] = new int [fib];
+        fibArr[0] = 0;
+        fibArr[1] = 1;
+        fibArr[2] = 1;
+
+        System.out.print("0");
+        System.out.print(", 1");
+
+        int index = 1;
+        while ( fibArr[index] <= fib){
+
+            index++;
+
+            fibArr[index] = fibArr[index-1] + fibArr[index-2];
+
+            if (fibArr[index] <= fib){
+                System.out.print(", "+ fibArr[index]);
+            } else {
+                break;
+            }
+
+        }
+
     }
 }
