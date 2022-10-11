6d81fedd52489f1a98c1452953421d6c1809daac
somayajannat9 <somayajannat9@gmail.com>
2022-10-11 16:55:09 -0400

    Completed find missing number.

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -1,18 +1,54 @@
 package math_problems;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class FindMissingNumber {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a method to find the missing number from the array.
      */
 
 
     public static void main(String[] args) {
-        int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
+        int[] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+        int[] input = {10, 2, 1, 4, 5, 3, 7, 8, 6};
 
+        int n = input.length + 1;
 
-        }
+        // Calculate Sum of N Number
+        // using Math formula n(n+1)/2
+        int sumOfNNumbers = sumOfAllNNumbers(n);
+
+        // Calculate Sum of all numbers in input array
+        int sumOfInputArray = sumOfInputArrayNumbers(input);
+
+        // Calculate missing number
+        // using subtraction
+        int missingNumber = sumOfNNumbers - sumOfInputArray;
+
+        // Print the Missing number
+        System.out.println("Missing number in an array is : "
+                + missingNumber);
     }
+
+    public static int sumOfAllNNumbers(int n){
+        int sum = (n*(n+1))/2;
+        return sum;
+    }
+
+    public static int sumOfInputArrayNumbers(int[] input){
+        int sum = 0;
+        for(int i=0; i < input.length ;i++){
+            sum = sum + input[i];
+        }
+        return sum;
+    }
+}
+
+
+
+
+
 
