20ff999c451a5cc4f1cc5d97bdc3dfbe79f2ff94
somayajannat9 <somayajannat9@gmail.com>
2022-10-11 16:20:27 -0400

    Completed LowestNumber.

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -1,13 +1,18 @@
 package math_problems;
 
+import java.lang.reflect.Array;
+
 public class FindMissingNumber {
 
     /** INSTRUCTIONS
      * Write a method to find the missing number from the array.
      */
 
+
     public static void main(String[] args) {
         int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
 
+
+        }
     }
-}
+
--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -11,7 +11,19 @@ public class LowestNumber {
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
 
+                //initialize with largest possible value
+                int smallest = Integer.MAX_VALUE;
+                //find smallest element of array
+                int index=0;
+                while(index<array.length) {
+                    //check if smallest is greater than element
+                    if(smallest>array[index]) {
+                        //update smallest
+                        smallest=array[index];
+                    }
+                    index++;
+                }
+                System.out.println("The smallest number is : "+ smallest);
+            }
+        }
 
-    }
-
-}
