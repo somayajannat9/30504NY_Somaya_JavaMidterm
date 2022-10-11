ef0f29f8a8391e3203450646ac32624964accdfd
somayajannat9 <somayajannat9@gmail.com>
2022-10-11 19:57:34 -0400

    Completed find lowest number.

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -1,5 +1,8 @@
 package math_problems;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class FindLowestDifference {
 
     /** INSTRUCTIONS
@@ -9,12 +12,20 @@ public class FindLowestDifference {
 
     public static void main(String[] args) {
 
-
+        FindLowestDifference obj = new FindLowestDifference();
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
+
+        int n = obj.lowestNum(array1, array2);
+        System.out.println(n);
+    }
+
+    public int lowestNum (int[] array1, int[] array2){
+        return Arrays.compare(array1,array2);
+    }
+
 
     }
 
 
 
-}
