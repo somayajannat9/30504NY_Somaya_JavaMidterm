107ec67ba16017d79a4374c51b23abd1adee5304
somayajannat9 <somayajannat9@gmail.com>
2022-10-12 16:06:46 -0400

    Completed String anagram.

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -1,11 +1,12 @@
 package string_problems;
-
+import java.util.Arrays;
 public class Anagram {
 
     /** INSTRUCTIONS
      * Write a Java Program, `isAnagram`, to check if any two strings are anagrams
      *
-     *  An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, using all of the
+     *  An anagram is a word or phrase formed by rearranging the letters of a
+     *  different word or phrase, using all of the
      *  original letters exactly once.
      *
      *         Ex: "CAT" & "ACT",
@@ -14,5 +15,39 @@ public class Anagram {
      */
 
     // Implement Here
+    public static void main(String[] args) {
+        String str1 = "Heart";
+        String str2 = "Earth";
 
+        // Set both strings to lower case because their letters will switch.
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        // check if the length of 2 words are the same
+        if(str1.length() == str2.length()) {
+
+            // convert strings to char array
+            char[] charArray1 = str1.toCharArray();
+            char[] charArray2 = str2.toCharArray();
+
+            // sort the char array
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            // if sorted char arrays are same
+            // then the string is anagram
+            boolean result = Arrays.equals(charArray1, charArray2);
+
+            if(result) {
+                System.out.println(str1 + " and " + str2 + " are anagram.");
+            }
+            else {
+                System.out.println(str1 + " and " + str2 + " are not anagram.");
+            }
+        }
+        else {
+            System.out.println(str1 + " and " + str2 + " are not anagram.");
+        }
+    }
 }
+
